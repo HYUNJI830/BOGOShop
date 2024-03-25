dd4533a38d07f4728165628da095942f44a6f6dd
hyunji94 <hyunji@datasolution.kr>
2024-03-25 17:20:23 +0900

    초기 셋팅

---
+++ src/main/java/cosmetics/BOGOShop/BogoShopApplication.java
@@ -0,0 +1,17 @@
+package cosmetics.BOGOShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BogoShopApplication {
+
+	public static void main(String[] args) {
+		Hello hello = new Hello();
+		hello.setData("hello");
+		String data = hello.getData();
+		System.out.println("data = " + data);
+		SpringApplication.run(BogoShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cosmetics/BOGOShop/BogoShopApplicationTests.java
@@ -0,0 +1,13 @@
+package cosmetics.BOGOShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BogoShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
