a6bb8f0425a6058db9f32c2ebc4d47c7e75ebeb6
hyunji94 <hyunji@datasolution.kr>
2024-03-27 15:08:28 +0900

    Feat : 회원 도메인 개발

---
+++ src/test/java/cosmetics/BOGOShop/service/MemberServiceTest.java
@@ -0,0 +1,61 @@
+package cosmetics.BOGOShop.service;
+
+import cosmetics.BOGOShop.domain.Member;
+import cosmetics.BOGOShop.repository.MemberRepository;
+import jakarta.persistence.EntityManager;
+import org.junit.Assert;
+import org.junit.Test;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    EntityManager em;
+
+    @Test
+    //@Rollback(value = false)
+    public void 회원가입() throws Exception{
+        //given(요건)
+        Member member = new Member();
+        member.setName("Lee");
+
+        //when(조건)
+        Long saveId = memberService.join(member);
+
+        //then(결과)
+        //em.flush();
+        Assert.assertEquals(member,memberRepository.findOne(saveId));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    //@Rollback(value = false)
+    public void 중복_회원_예외() throws Exception{
+        //given(요건)
+        Member member1 = new Member();
+        member1.setName("Lee");
+
+        Member member2 = new Member();
+        member2.setName("Lee");
+
+        //when(조건)
+        memberService.join(member1);
+        memberService.join(member2); //예외가 발생
+        //then(결과)
+        fail("예외가 발생해야 한다.");
+    }
+}
